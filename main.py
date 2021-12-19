9f2b63b95a7707e387046395c64a557e5a83979b
Yessine Zghal <74101918+yessine-zghal@users.noreply.github.com>
2021-12-19 20:50:26 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNet('yolov3.weights', 'yolov3.cfg')
+classes = []
+with open('coco.names', 'r') as f:
+    classes = f.read().splitlines()
+
+#cap = cv2.VideoCapture('test_video.mp4')
+cap = cv2.VideoCapture(0)
+while True:
+    _, img = cap.read()
+    #img = cv2.imread('test.jpg')
+    height, width, _ = img.shape
+    blob = cv2.dnn.blobFromImage(img, 1/255,(416,416), (0,0,0), swapRB=True, crop=False)
+    net.setInput(blob)
+    output_layers_names = net.getUnconnectedOutLayersNames()
+    layersOutputs = net.forward(output_layers_names)
+
+
+    boxes = []
+    confidences = []
+    class_ids = []
+
+    for output in layersOutputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                centre_x = int(detection[0]*width)
+                centre_y = int(detection[1]*height)
+                w = int(detection[2]*width)
+                h = int(detection[3]*height)
+
+                x = int(centre_x - w/2)
+                y = int(centre_y - h/2)
+
+                boxes.append([x, y, w, h])
+                confidences.append((float(confidence)))
+                class_ids.append(class_id)
+
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    font = cv2.FONT_HERSHEY_PLAIN
+    colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+    if len(indexes> 0):
+        for i in indexes.flatten():
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = str(round(confidences[i],2))
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
+            cv2.putText(img, label + " " + confidence, (x, y+20), font, 2, (255, 255, 255), 2)
+
+
+
+
+
+
+
+
+    cv2.imshow('Image', img)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.relearse()
+cv2.destroyWindow()
